49ab9d9fab8b0f12ec86d9034301530e7bafa35d
Eugene Khudoy <eugene.khudoy@gmail.com>
2015-02-02 23:03:54 +0200

    Server initialization handler added

--- src/iwc/Server.js
+++ src/iwc/Server.js
@@ -7,6 +7,9 @@
         SJ.copy(me, serverConfig);
         SJ.copy(me, privateConfig);
         SJ.lock(serverId, function () {
+            if (me.initServer) {
+                me.initServer();
+            }
             me.updateServerDescription(serverConfig);
             SJ.iwc.EventBus.on('servercall_' + me._serverId, me.onServerCall, me);
         });
